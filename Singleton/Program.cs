7a9c27d60916b4e54ad60880c3d3b99413a65039
isbdnt <isbdnt@outlook.com>
2018-06-16 20:50:00 +0800

    add singleton pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thread-safe lazy instantiation");
+            Singleton singleton = Singleton.Instance;
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton Instance => lazy.Value;
+
+        private Singleton()
+        {
+            Console.WriteLine("Singleton created");
+        }
+    }
+}
