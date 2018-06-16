8406014c3a56aa33bc98e09539c60cb242e78c37
isbdnt <isbdnt@outlook.com>
2018-06-16 20:27:04 +0800

    add observer pattern

---
+++ Observer/LocationTracker .cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class LocationTracker : IObservable<Location>
+    {
+        private class Unsubscriber : IDisposable
+        {
+            private List<IObserver<Location>> observers;
+            private IObserver<Location> observer;
+
+            public Unsubscriber(List<IObserver<Location>> observers, IObserver<Location> observer)
+            {
+                this.observers = observers;
+                this.observer = observer;
+            }
+
+            public void Dispose()
+            {
+                if (observer != null && observers.Contains(observer))
+                    observers.Remove(observer);
+            }
+        }
+
+        private List<IObserver<Location>> observers;
+
+        public LocationTracker()
+        {
+            observers = new List<IObserver<Location>>();
+        }
+
+        public IDisposable Subscribe(IObserver<Location> observer)
+        {
+            if (!observers.Contains(observer))
+                observers.Add(observer);
+            return new Unsubscriber(observers, observer);
+        }
+
+        public void TrackLocation(Nullable<Location> loc)
+        {
+            foreach (var observer in observers)
+            {
+                if (!loc.HasValue)
+                    observer.OnError(new LocationUnknownException());
+                else
+                    observer.OnNext(loc.Value);
+            }
+        }
+
+        public void EndTransmission()
+        {
+            foreach (var observer in observers.ToArray())
+                if (observers.Contains(observer))
+                    observer.OnCompleted();
+
+            observers.Clear();
+        }
+    }
+}
