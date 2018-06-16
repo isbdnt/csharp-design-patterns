eba17610c96c5aeff2fd910c62943fa8916fd839
isbdnt <isbdnt@outlook.com>
2018-06-16 21:14:47 +0800

    add strategy pattern

---
+++ Strategy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StrategyExecutor executor = new StrategyExecutor(new Strategy1());
+
+            executor.Execute();
+            executor.ChangeStrategy(new Strategy2());
+            executor.Execute();
+            executor.ChangeStrategy(new Strategy1());
+            executor.Execute();
+        }
+    }
+}
---
+++ Strategy/StrategyExecutor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class StrategyExecutor
+    {
+        IStrategy strategy;
+
+        public StrategyExecutor(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public void ChangeStrategy(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public void Execute()
+        {
+            strategy.Execute();
+        }
+    }
+}
