a4ad8f4a9b63d7098360aaa01979b4941e9d8dd3
isbdnt <isbdnt@outlook.com>
2018-06-19 11:03:09 +0800

    add proxy pattern

---
+++ Proxy/ProxyCar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class ProxyCar : ICar
+    {
+        private Driver driver;
+        private ICar realCar;
+
+        public ProxyCar(Driver driver)
+        {
+            this.driver = driver;
+            realCar = new Car();
+        }
+
+        public void DriveCar()
+        {
+            if (driver.Age < 16)
+                Console.WriteLine("Sorry, the driver is too young to drive.");
+            else
+                realCar.DriveCar();
+        }
+    }
+
+}
